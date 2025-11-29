e8348c0ead5246b31cb30102ff99a97491da58ee
DimitrijeA <53818425+SavkoDinamitas@users.noreply.github.com>
2025-11-29 19:22:29 +0100

    add all tests for join generation

---
+++ src/test/java/QueryBuilderTest.java
@@ -0,0 +1,41 @@
+import discovery.test2.Airplane;
+import discovery.test2.Crew;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import raf.thesis.metadata.scan.MetadataScanner;
+import raf.thesis.query.QueryBuilder;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class QueryBuilderTest {
+    @BeforeAll
+    public static void fillMetadata(){
+        MetadataScanner ms = new MetadataScanner();
+        ms.discoverMetadata("discovery.test2");
+    }
+
+    @Test
+    public void testSingleJoinGeneration(){
+        QueryBuilder qb = new QueryBuilder();
+        String check = qb.select(Crew.class).join("pilot").generateJoinClauses();
+        assertEquals("INNER JOIN pilots AS \"pilot\" ON ((\"pilot\".pilotid) = (\"%root\".pilotid))\n", check);
+    }
+
+    @Test
+    public void testManyToManyJoinGeneration(){
+        QueryBuilder qb = new QueryBuilder();
+        String check = qb.select(Airplane.class).join("flights").generateJoinClauses();
+        assertEquals("INNER JOIN airplanes_flights AS \"airplanes_flights\" ON ((\"airplanes_flights\".id) = (\"%root\".id))\n" +
+                "INNER JOIN flights AS \"flights\" ON ((\"flights\".flightnumber) = (\"airplanes_flights\".flightnumber))\n", check);
+    }
+
+    @Test
+    public void testInDepthJoinGeneration(){
+        QueryBuilder qb = new QueryBuilder();
+        String check = qb.select(Airplane.class).join("flights").join("flights.crew").join("flights.crew.pilot").generateJoinClauses();
+        assertEquals("INNER JOIN airplanes_flights AS \"airplanes_flights\" ON ((\"airplanes_flights\".id) = (\"%root\".id))\n" +
+                "INNER JOIN flights AS \"flights\" ON ((\"flights\".flightnumber) = (\"airplanes_flights\".flightnumber))\n" +
+                "INNER JOIN crews AS \"flights.crew\" ON ((\"flights.crew\".crewid) = (\"flights\".crewid))\n" +
+                "INNER JOIN pilots AS \"flights.crew.pilot\" ON ((\"flights.crew.pilot\".pilotid) = (\"flights.crew\".pilotid))\n", check);
+    }
+}
